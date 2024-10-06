2e582b9cffcb4a5d9607fa3f5abf9e4863583817
Dhiman Majumdar <dhimanmajumdar08233@gmail.com>
2024-10-07 01:15:49 +0530

    done all

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,3 @@
-// constants.js
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg";
 export const USER_ICON =
@@ -7,12 +6,10 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MTdjNTk3OGYwZWEzM2VkOWMxNjUwZjk0NjUyZWQzMiIsIm5iZiI6MTcyODEzMDk0OC40Nzg1MzUsInN1YiI6IjY3MDEyY2ExNzgzMGMxMzAxZTdjZmViZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iRWoiNB0tZYj0ibvS8l7sf5Eq2pAJu1UeN8R4--_mmw",
   },
 };
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
-export const SUPPORTED_LANGUAGES = [
-  { identifier: "en", name: "English" },
-  { identifier: "hin", name: "Hindi" },
-];
-export const GEMINI_API_KEY ="AIzaSyArQ-XueMwqFhwqne7C3SNGlUaoN732ShM";
+export const SUPPORTED_LANGUAGES=[{identifier:"en",name:"English"},{identifier:"hin",name:"Hindi"}];
+export const GEMINI_API_KEY="AIzaSyArQ-XueMwqFhwqne7C3SNGlUaoN732ShM";
