c88055594e3af1b630df1fde3824b4fe874687d1
Dhiman Majumdar <dhimanmajumdar08233@gmail.com>
2025-02-16 13:21:39 +0530

    changed api key of gemini and changed model too

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -12,4 +12,4 @@ export const API_OPTIONS = {
 };
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 export const SUPPORTED_LANGUAGES=[{identifier:"en",name:"English"},{identifier:"hin",name:"Hindi"}];
-export const GEMINI_API_KEY="AIzaSyArQ-XueMwqFhwqne7C3SNGlUaoN732ShM";
+export const GEMINI_API_KEY="AIzaSyAe_CzETgt-iR_T6YIP4NjiHFuYaOxtexc";
--- src/utils/openAi.jsx
+++ src/utils/openAi.jsx
@@ -2,6 +2,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GEMINI_API_KEY } from "./constants";
 
 const genAI=new GoogleGenerativeAI(GEMINI_API_KEY);
-const model=genAI.getGenerativeModel({model:"gemini-1.5-flash"});
+const model=genAI.getGenerativeModel({model:"gemini-2.0-flash"});
 
 export default model;
