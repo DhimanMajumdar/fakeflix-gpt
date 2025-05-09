db5d9966ee2057230035f9f274b78c0a76bdad1e
Dhiman Majumdar <dhimanmajumdar08233@gmail.com>
2025-05-10 02:59:49 +0530

    validation msg added

--- src/utils/validate.jsx
+++ src/utils/validate.jsx
@@ -1,9 +1,12 @@
 export const checkValidateData = (email, password) => {
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-    if (!isEmailValid) return "Email ID is not valid";
-    if (!isPasswordValid) return "Password is not valid";
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPasswordValid) {
+    return "Password must contain at least 8 characters, including uppercase, lowercase, number, and special character";
+  }
 
-    return null;
-}
+  return null;
+};
